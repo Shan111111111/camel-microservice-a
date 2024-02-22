d894826c69daf5a73b0414f5f236f61d7fe82990
Shantesh <shanthu1804@gmail.com>
2024-02-23 01:17:59 +0530

    docker commands to get the ActiveMQ running on the docker as container: docker run -p 61616:61616 -p 8161:8161 rmohr/activemq and Dummy test file with some comments too promoted

--- src/main/java/com/shantesh/camelmicroservicea/CamelMicroserviceAApplication.java
+++ src/main/java/com/shantesh/camelmicroservicea/CamelMicroserviceAApplication.java
@@ -8,6 +8,7 @@ public class CamelMicroserviceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamelMicroserviceAApplication.class, args);
+		System.out.println("Learning camel routing, from and to to endpoints, timer-to-log, fileyafolder-to- fileyafolder, queue-to-database");
 	}
 
 }
